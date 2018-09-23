bf758baca528b9bb40e11fa2010a1a56e06cdb34
Willian-Girao <williansoaresgirao@gmail.com>
2018-09-22 22:35:45 -0300

    Server basic setup + basic middlewares

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,19 @@
-console.log('Hi')
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('combined'))
+
+app.get('/status', (req, res) => {
+  res.status(200).send({
+    message: 'hello world'
+  })
+})
+
+app.listen(process.env.SERVER_PORT || 8081)
+console.log('Server up and running on port 8081')
